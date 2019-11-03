9b377c4be84bb656c1974c29c05e3b9fa5349812
AmanGodiyal <amangodiyal@gmail.com>
2019-11-03 12:33:09 +0530

    resolved conflict messages

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 function hello(){
-<<<<<<< HEAD
     console.log('I am in login Hi there!!');
-=======
     console.log('this message added to master. to create conflict');
     let x =0; //in master;
->>>>>>> master
 }
